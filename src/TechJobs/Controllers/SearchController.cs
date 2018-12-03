05d44e338b9581ee9df1334e2d1b9a16ffbc68bd
Steven Rausch <stevenrausch44@gmail.com>
2018-12-03 16:43:27 -0600

    finished mvc

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -5,7 +5,7 @@ using TechJobs.Models;
 namespace TechJobs.Controllers
 {
     public class SearchController : Controller
-    {
+    {[HttpGet]
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
@@ -13,6 +13,43 @@ namespace TechJobs.Controllers
             return View();
         }
 
+        [Route("/Search")]
+        [HttpPost]
+        
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchTerm == null)
+            {
+                if (searchType.Equals("all"))
+                {
+                    List<Dictionary<string, string>> jobs = JobData.FindAll();
+                    ViewBag.columns = ListController.columnChoices;
+                    ViewBag.jobs = jobs;
+
+                    
+                }
+                return View("Index");   
+            }
+          
+            
+
+            else if (searchType.Equals("all"))
+
+            {
+                
+                List<Dictionary<string, string>> Items = JobData.FindByValue(searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.jobs = Items;
+                return View("Index");
+            
+            }
+            else
+            { List<Dictionary<string, string>> results = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.jobs = results;
+                return View("Index");
+            }
+        }
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
